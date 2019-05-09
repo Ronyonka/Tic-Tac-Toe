5e09ed27d836ef3d3c5305321cb910145207c23e
Ronyonka <rononyonka@gmail.com>
2019-05-09 14:25:48 +0300

    added simple AI that allows the machine to play

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 var origBoard;
-const huPlayer = 'o';
+const huPlayer = 'O';
 const aiPlayer = 'X';
 const winCombos = [
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [2,5,8],
-  [1,4,7],
-  [0,4,8],
-  [6,4,2]
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
 ]
 
 const cells = document.querySelectorAll('.cell');
 startGame();
 
 function startGame() {
-  document.querySelector('.endgame').style.display = "none"
-  origBoard = Array.from(Array(9).keys());
-  for(var i=0; i<cells.length;i++){
-    cells[i].innerText = '';
-    cells[i].style.removeProperty('background-color');
-    cells[i].addEventListener('click', turnClick, false);
-  }
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
 }
 
 function turnClick(square) {
-  turn(square.target.id, huPlayer)
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, huPlayer)
+		if (!checkTie()) turn(bestSpot(), aiPlayer);
+	}
 }
 
 function turn(squareId, player) {
-  origBoard[squareId] = player;
-  document.getElementById(squareId).innerText = player;
-  let gameWon = checkWin(origBoard, player)
-  if (gameWon) gameOver(gameWon)
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
 }
 
 function checkWin(board, player) {
@@ -50,10 +53,37 @@ function checkWin(board, player) {
 }
 
 function gameOver(gameWon) {
-  for(let index of winCombos[gameWon.index]){
-    document.getElementById(index).style.backgroundColor = gameWon.player == huPlayer ? "blue":"red";
-  }
-  for (var i=0; i < cells.length; i++) {
-    cells[i].removeEventListener('click', turnClick, false);
-  }
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == huPlayer ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == huPlayer ? "You Got Lucky" : "Hold this L you loser!!!!");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return emptySquares()[0];
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("At least You Tried")
+		return true;
+	}
+	return false;
 }
